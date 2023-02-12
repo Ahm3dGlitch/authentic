6d01c9f11c1261a4718c6a8e8e6de1e07791b591
Ahm3dGlitch <elricxx@gmail.com>
2023-02-12 17:17:16 +0600

    npm

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-console.log('Hello world');
+import express from 'express'
+// const express = require('express')
+
+const app = express()
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use( express.json())
+
+const users = [
+    {
+        id: 1, 
+        name: "IIII"
+    },
+    {
+        id: 2, 
+        name: "dfgjhndgkjn"
+    },
+    {
+        id: 3, 
+        name: "dhhdh"
+    }
+];
+
+app.get('/user/:id', (req, res, next)=>{
+
+    console.log(req.params)
+
+    res.json(users.find(i => i.id === parseInt(req.params.id)))
+
+});
+
+app.post('/users', (req, res, next)=>{
+
+    console.log(req.body)
+    // users.push(req.body);
+
+res.json(users)
+
+});
+
+app.post('/user', (req, res, next)=>{
+
+    console.log(req.body)
+    users.push(req.body);
+
+res.json(users)
+
+});
+
+app.get('/user', (req, res, next)=>{
+    console.log(req.headers['authorization'])
+    console.log(req.query)
+    users.push(req.query)
+
+    res.json(users)
+
+});
+
+app.get('/', (req, res, next)=>{
+
+    console.log(req)
+
+    res.json(users)
+
+});
+
+
+app.listen('3000', ()=> {
+    console.log("My port is 3000")
+});
